3956d3059ae822786e13c7629911dd36ea36bd5f
GTHell <sosxradar@gmail.com>
2018-07-11 03:06:19 +0700

    Modify move seeder to database, migration to database and models to app Adopt MVC pattern

---
+++ server/database/seeders/20180710194434-demo-post.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Posts', [{
+      title: 'Testing world',
+      content: 'Helll world this is the best thing ever happen to ma lafe'
+    }], {})
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+   queryInterface.bulkDelete('Posts', null, {})
+  }
+};
